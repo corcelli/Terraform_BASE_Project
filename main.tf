8737ff601213c8b2dfab29da8d4c522168f064c1
corcelli <tiagocorcelli@gmail.com>
2021-05-22 16:25:19 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+
+resource "aws_instance" "instance01" {
+
+  ami           = var.id_instance_ami
+  instance_type = var.type_instance_ami
+  key_name      = "Keys_DockerServerFastDeliveryAWS"
+  lifecycle {
+    ignore_changes = [
+      user_data
+    ]
+  }
+
+  user_data = <<-EOF
+        #!/bin/bash
+        apt-get install git -y
+        git clone https://github.com/corcelli/devops.git
+        cd devops
+        chmod +x install_docker_debian_v1.sh
+        ./install_docker_debian_v1.sh
+        EOF
+
+  root_block_device {
+    volume_size = "110"
+  }
+  tags = {
+    nome    = "LinuxAWS"
+    projeto = "Trinity"
+  }
+}
+
